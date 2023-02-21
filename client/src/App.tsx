5169ff8de0d885afbdeced3c4682c5cd46571ed5
tevin davis <aod.td1105@gmail.com>
2023-02-21 06:05:12 -0500

    Able to add users to the db

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -33,18 +33,36 @@ return request;
 
 function App() {
     const authProvider: AuthProvider = {
-        login: ({ credential }: CredentialResponse) => {
+        login: async ({ credential }: CredentialResponse) => {
             const profileObj = credential ? parseJwt(credential) : null;
 
-            if (profileObj) {
+            if(profileObj) {
+              const response = await fetch('http://localhost:8080/api/v1/users', {
+                method: 'POST',
+                headers:{ 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                  name: profileObj.name,
+                  email: profileObj.email,
+                  avatar: profileObj.picture
+                })
+              })
+
+              const data = await response.json();
+
+              if(response.status === 200) {
                 localStorage.setItem(
-                    "user",
-                    JSON.stringify({
-                        ...profileObj,
-                        avatar: profileObj.picture,
-                    }),
+                  "user",
+                  JSON.stringify({
+                      ...profileObj,
+                      avatar: profileObj.picture,
+                      userid: data._id
+                  }),
                 );
-            }
+              } else {
+                return Promise.reject();
+              }
+            } 
+
             localStorage.setItem("token", `${credential}`);
             return Promise.resolve();
         },
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,28 @@
 import User from '../mongodb/models/user.js';
 
 const getAllUsers = async (req, res) => {};
-const createUser = async (req, res) => {};
+
+const createUser = async (req, res) => {
+  try {
+    const { name, email, avatar } = req.body;
+
+    const userExist = await User.findOne({ email });
+  
+    if(userExist) return res.status(200).json(userExist);
+  
+    const newUser = await User.create({
+      name,
+      email,
+      avatar,
+    });
+  
+    return res.status(200).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+
+};
+
 const getUserInfoByID = async (req, res) => {};
 
 export {
